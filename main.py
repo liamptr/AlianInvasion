08d8b2bff6c94acb5f73bf9cfb18702315f715a5
test <test@yahoo.com>
2022-03-12 01:01:53 -0800

    movment is added

--- main.py
+++ main.py
@@ -36,7 +36,26 @@ def draw(red,yellow):
      win.blit(bg,(0,0))
      win.blit(red_space_ship,(red.x,red.y))
      win.blit(yellow_space_ship,(yellow.x,yellow.y))
-     
+#set movment     
+def red_movment(keys,red):
+    if keys[pygame.K_a] and red.x - 5 > 0:
+        red.x -= 5
+    if keys[pygame.K_d] and red.x + 5 < width // 2:
+        red.x += 5
+    if keys[pygame.K_w] and red.y -5 > 0 :
+        red.y -= 5
+    if keys[pygame.K_s]  and red.y + 5 < height - spaceship_height :
+        red.y += 5
+        
+def yellow_movment(keys,yellow):
+    if keys[pygame.K_LEFT] and yellow.x - 5 > width // 2:
+        yellow.x -= 5
+    if keys[pygame.K_RIGHT] and yellow.x + 5 < width - spaceship_width :
+        yellow.x += 5
+    if keys[pygame.K_UP] and yellow.y -5 > 0 :
+        yellow.y -= 5
+    if keys[pygame.K_DOWN]  and yellow.y + 5 < height - spaceship_height :
+        yellow.y += 5
     
      
 
@@ -54,7 +73,10 @@ def main():
             if event == pygame.QUIT:
                 run=False
         draw(red,yellow)
-         
+        keys=pygame.key.get_pressed()
+        red_movment(keys, red)
+        yellow_movment(keys,yellow)
+        
         pygame.display.update()       
     pygame.quit()     
                  
