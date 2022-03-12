8fdc9bdb92d1b62a24a62fe404c10e3d03529b2c
test <test@yahoo.com>
2022-03-12 00:36:37 -0800

    space_ship added

--- main.py
+++ main.py
@@ -12,30 +12,48 @@ width , height = 900 , 500
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption('space')
 
+spaceship_width, spaceship_height = 55, 40
 #set fps
 fps=60
 
 #give background
 bg = pygame.transform.scale(pygame.image.load(
     'C:\\Users\\ssssa\\Desktop\\AI remote\\assets\\space.png'), (width , height))
+#give spaceships
 
-def draw():
+red_space_ship=pygame.transform.rotate(
+    pygame.transform.scale(
+        pygame.image.load('C:\\Users\\ssssa\\Desktop\\AI remote\\assets\\spaceship_red.png'), 
+        (spaceship_width, spaceship_height)), 90)
+yellow_space_ship=pygame.transform.rotate(
+    pygame.transform.scale(
+        pygame.image.load('C:\\Users\\ssssa\\Desktop\\AI remote\\assets\\spaceship_yellow.png'), 
+        (spaceship_width, spaceship_height)), 270)
+
+
+
+def draw(red,yellow):
      win.blit(bg,(0,0))
+     win.blit(red_space_ship,(red.x,red.y))
+     win.blit(yellow_space_ship,(yellow.x,yellow.y))
      
-     
+    
      
 
     
 def main():
     clock=pygame.time.Clock()
     run=True
+    red=pygame.Rect(width//8, height//2, 300, 400)
+    yellow=pygame.Rect(width - (width//8),height//2, width, height)
     
     while run:
         clock.tick(fps)
+        
         for event in pygame.event.get():
             if event == pygame.QUIT:
                 run=False
-        draw()
+        draw(red,yellow)
          
         pygame.display.update()       
     pygame.quit()     
